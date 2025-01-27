4191bc4e229cc8532e8bb0b54dd0824a3ac055c9
sanvirahman <sheikh15-3700@diu.edu.bd>
2025-01-27 18:23:05 +0600

    init now

--- myApp/urls.py
+++ myApp/urls.py
@@ -12,6 +12,7 @@ from .views import (
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
+    path('create/', BlogCreateView.as_view(), name='create'),
     path('create/<int:pk>/', BlogCreateView.as_view(), name='create'),
     path('delete/<int:pk>/', BlogDeleteView.as_view(), name='delete'),
     path('update/<int:pk>/', BlogUpdateView.as_view(),name="update"),
